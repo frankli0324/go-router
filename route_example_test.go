0942203343ba38d9d767442d42ec07abf17a442f
Frank <frankli0324@hotmail.com>
2025-08-02 04:03:01 +0800

    feat: make examples runnable

--- route_example_test.go
+++ route_example_test.go
@@ -8,7 +8,8 @@ func ExampleNewRouter() {
 	r := NewRouter[any]()
 	r.Set("/api/v1/hello", 1)
 	r.Set("/api/v1/world", 2)
-	fmt.Println(r.Get("/api/v1/hello") == 1)
+	fmt.Println(r.Get("/api/v1/hello"))
+	// Output: 1
 }
 
 func ExampleRouter_Get() {
@@ -17,11 +18,16 @@ func ExampleRouter_Get() {
 	r.Set("/1", 1)
 	r.Set("/sub/test", "literals have highest prio")
 	r.Set("/sub/{a}", "2")
-	r.Set("/{anything:*}", 3.0)
-	fmt.Println(r.Get("/sub/test") == "literals have highest prio")
-	fmt.Println(r.Get("/sub/other") == "2")
-	fmt.Println(r.Get("/sub/other/") == 3.0) // because it doesn't match /sub/{a} due to an extra trailing slash
-	fmt.Println(r.Get("/") == 3.0)           // "anything" could be empty, same as fasthttp
+	r.Set("/{anything:*}", 3.1)
+	fmt.Println(r.Get("/sub/test"))
+	fmt.Println(r.Get("/sub/other"))
+	fmt.Println(r.Get("/sub/other/")) // because it doesn't match /sub/{a} due to an extra trailing slash
+	fmt.Println(r.Get("/"))           // "anything" could be empty, same as fasthttp
+	// Output:
+	// literals have highest prio
+	// 2
+	// 3.1
+	// 3.1
 }
 
 func ExampleRouter_GetParam() {
@@ -29,6 +35,9 @@ func ExampleRouter_GetParam() {
 	r.Set("/", 1)
 	r.Set("/{a}", "2")
 	param := make(map[string]string)
-	fmt.Println(r.GetParam("/test", param) == "2")
-	fmt.Println(param["a"] == "test")
+	fmt.Println(r.GetParam("/test", param))
+	fmt.Println(param["a"])
+	// Output:
+	// 2
+	// test
 }
